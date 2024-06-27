f018062c80816eb4ee09a534a2d927d65b486d85
ZhiJun4030 <ZhiJun4030@outlook.com>
2024-06-27 23:47:13 +0800

    New file created by ZJ

--- src/main/java/com/cac/mpn/Mod_Main.java
+++ src/main/java/com/cac/mpn/Mod_Main.java
@@ -12,7 +12,7 @@ public class Mod_Main
 {
     public static final String MODID = "mpn";
     public static final String NAME = "Minecraft Project Nexues";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "Test-0.1";
 
     private static Logger logger;
 
