a8f13f32a63ff4b454fa5f9220807306ec32f4b8
ZhiJun4030 <ZhiJun4030@outlook.com>
2024-06-28 11:36:32 +0800

    updated by ZJ

---
+++ src/main/java/com/cac/mpn/item/RegisterItem.java
@@ -0,0 +1,20 @@
+package com.cac.mpn.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+class RegisterItem {
+
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(new Item().setRegistryName("data_knife").setCreativeTab(ModTabs.MPN_TAB));
+        //第一个物品，暂时未添加贴图
+    }
+
+}
