911c6b6327f45cfcac34d5235b7c6398985697f0
ZhiJun4030 <ZhiJun4030@outlook.com>
2024-06-30 19:31:51 +0800

    格式

--- src/main/java/com/cac/mpn/item/Item_Swords/DataKnife.java
+++ src/main/java/com/cac/mpn/item/Item_Swords/DataKnife.java
@@ -1,15 +1,21 @@
 package com.cac.mpn.item.Item_Swords;
 
+// 导入所需的包
 import com.cac.mpn.item.RegisterItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemSword;
 import net.minecraftforge.common.util.EnumHelper;
 
+// 定义名为 DataKnife 的类，继承 ItemSword 类
 public class DataKnife extends ItemSword {
-    public static final Item.ToolMaterial DATA_SWORD= EnumHelper.addToolMaterial("DATA",3, 255, 5.5F, 1.5F, 14);
+
+    // 定义工具材料常量，参数分别为名称、等级、耐久度、效率（采掘速度）、攻击伤害、附魔能力
+    public static final Item.ToolMaterial DATA_SWORD = EnumHelper.addToolMaterial("DATA", 3, 255, 5.5F, 1.5F, 14);
+
+    // 构造方法
     public DataKnife() {
-        super(DATA_SWORD);
-        this.setUnlocalizedName("data_knife");
-        this.setRegistryName("data_knife");
+        super(DATA_SWORD); // 调用父类构造方法，传入工具材料(请忽略警告)
+        this.setUnlocalizedName("data_knife"); // 设置未本地化的名称
+        this.setRegistryName("data_knife"); // 设置注册名称
     }
 }
--- src/main/java/com/cac/mpn/item/RegisterItem.java
+++ src/main/java/com/cac/mpn/item/RegisterItem.java
@@ -2,25 +2,20 @@ package com.cac.mpn.item;
 
 import com.cac.mpn.item.Item_Swords.DataKnife;
 import net.minecraft.item.Item;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
-
-@Mod.EventBusSubscriber
+// 定义一个名为 RegisterItem 的类，用于注册游戏中的物品
 public class RegisterItem {
 
-
-
-
+    // 定义一个静态的 DataKnife 实例
     public static final DataKnife DATA_KNIFE = new DataKnife();
+
+    // 在物品注册事件中注册 DataKnife 实例
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
         registry.register(DATA_KNIFE);
-
     }
-
 }
