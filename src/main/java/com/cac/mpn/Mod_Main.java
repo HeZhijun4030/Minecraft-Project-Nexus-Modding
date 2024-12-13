7e84836bb7cc3abbb964041611bec29afeadbd17
CIPEA101 <2652449949@qq.com>
2024-12-13 21:56:37 +0800

    Update to 0.3.0

--- src/main/java/com/cac/mpn/Mod_Main.java
+++ src/main/java/com/cac/mpn/Mod_Main.java
@@ -10,9 +10,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Mod_Main.MODID, name = Mod_Main.NAME, version = Mod_Main.VERSION)
 public class Mod_Main
 {
-    public static final String MODID = "Minecraft Project Nexues";
+    public static final String MODID = "Minecraft_Project_Nexues";
     public static final String NAME = "Minecraft Project Nexues";
-    public static final String VERSION = "0.2.1";
+    public static final String VERSION = "0.3.0";
 
     private static Logger logger;
 
